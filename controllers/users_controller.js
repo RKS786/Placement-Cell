97f04e996e7df9e70ab7664713194779ece519cc
Ranik <er.rahul0314@gmail.com>
2024-01-24 15:01:23 +0530

    created user profile page with list of students and also styed it

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,34 +1,40 @@
 console.log('user controller loaded');
 
 const User = require('../models/users_db');
+const Student = require('../models/student_db');
 
 // Controller function to sign in for user
-module.exports.signIn = function(req, res) {
-    
-     //steps to authenticate
-    // check whether the user exists or not
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            // handle user found
-            console.log(user);
-            if (user) {
-                if (user.password !== req.body.password) {
-                   return Promise.reject("password does not match");
-                }
-                
-                return res.render('user_profile', {
-                                        title: 'User profile',
-                                        user: user
-                                    });
-            } else {
-                return Promise.reject("user does not exist");
-            }
-        })
-        .catch(err => {
-            console.error("Error in user sign in:", err);
+module.exports.signIn = async function(req, res) {
+    try {
+        console.log("in createStudent controller function");
+
+        // Check whether the user exists or not
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            console.log('User does not exist');
             return res.redirect('back');
+        }
+
+        // Validate the password
+        if (user.password !== req.body.password) {
+            console.log('Password does not match');
+            return res.redirect('back');
+        }
+
+        // Retrieve all students
+        const students = await Student.find({}).exec();
+        console.log(students);
+
+        return res.render('user_profile', {
+            title: 'User profile',
+            user: user,
+            students: students
         });
-    
+    } catch (error) {
+        console.error("Error in user sign in:", error);
+        return res.redirect('back');
+    }
 };
 
 
@@ -75,3 +81,36 @@ module.exports.signUp = function(req, res){
         title:"Placement Cell | Sign Up"
     });
 };
+
+//Controller function to add Students
+module.exports.addStudent = function(req, res){
+    return res.render('add_students');
+}
+
+//Controller function to render user's profile page
+module.exports.profile = function(req, res){
+
+    return res.render('user_profile', )
+}
+
+//Controller function to create Student
+module.exports.createStudent = async function (req, res) {
+        try {
+            console.log("in createStudent controller function");
+            const existingStudent = await Student.findOne({ email: req.body.email });
+    
+            if (existingStudent) {
+                console.log('Email already exists');
+                return res.redirect('back');
+            }
+    
+            const newStudent = await Student.create({...req.body});
+    
+            console.log('Student created successfully:', newStudent);
+            return res.render('user_profile');
+
+        } catch (error) {
+            console.error('Error in creating student:', error);
+            return res.redirect('back');
+        }
+    };
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,13 @@ console.log('user router loaded');
 const userController = require('../controllers/users_controller');
 
 router.get('/sign-up', userController.signUp);
+router.get('/profile', userController.profile);
+router.get('/add-student', userController.addStudent);
+
 
 router.post('/sign-in',  userController.signIn);
 router.post('/create', userController.create);
+router.post('/create-student', userController.createStudent);
 
 
 module.exports = router;
